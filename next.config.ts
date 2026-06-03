8ca3f102b61ba4f8691612a951755651bd68cb07
Adriel Ventura <adrielvent5@gmail.com>
2026-06-03 09:19:16 -0400

    Add GitHub Actions workflow for pages deployment and update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  // GitHub Pages uses a subpath, e.g. /vnua-apparel
+  basePath: isGithubActions ? '/vnua-apparel' : '',
 };
 
 export default nextConfig;
+
